e6e5f34c1afa87a08deefff81149db28bb4a8135
Mouse10 <lj7e@wetigers.org>
2018-10-24 14:39:12 -0700

    Add more notes

---
+++ Basic Python Notes.py
@@ -0,0 +1,47 @@
+print("Hello World!")
+
+# Apparently I'm going to slow, so I will speed up
+# This is a comment
+# This has no effect on the code
+# This is used for a variety of things, such as
+# 1. Making personal notes about my code
+# 2. Commenting out code that does not work
+
+print("Notice what is happening here.")
+print()  # This creates a new line
+print()  # Do you notice the underline here?
+# Hover over top it to see what is wrong.\
+
+# Math
+print (5+3)
+print (5-3)
+print (4*5)
+print (6/5)
+
+# semi-advanced match
+print ("figure this out..")
+print (6//4)
+print (12//3)# This will only give me whole number
+print (9//4)
+print()
+
+print ("figure this out too...")
+print (6%4)
+print (5%3)
+print (9%4)
+
+# Variables
+car_name = "Wiebe moblie" #string
+cat_type = "tesla"
+car_cylinders = 16 #interger hmr
+car_miles_per_gallon=0/01 #float
+
+print ("I have a car called %s. it's pretty nice. ") % car_name)
+print ("it has %d cylinders, but gets %f mpg" % (car_cylinders, car_miles_per_gallon))
+
+# Taking input
+name = input ("what is your name")
+print ("hello %s" % name)
+
+age = input ("how old are you")
+print (%s? you belong in a museum!" %age)
