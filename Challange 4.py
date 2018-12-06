427f3ca6fd9d5c21d77a38a9149258b25b49ad14
Mouse10 <lj7e@wetigers.org>
2018-12-06 14:37:29 -0800

    challange 4

---
+++ Challange 4.py
@@ -0,0 +1,13 @@
+import random
+
+number = random.randint(-100,100)
+print(number)
+
+if number (-1, -100):
+    print("This number is negative")
+
+    if number (0):
+        print("it isn't negative or positive its zero")
+
+        if number (1,100):
+            print("this number is a positive")
