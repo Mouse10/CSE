ab3d18f088414d7701fdb54e172632db5eca0894
Mouse10 <44381333+Mouse10@users.noreply.github.com>
2019-01-08 14:40:21 -0800

    sem 2

---
+++ Semester 2 notes.py
@@ -0,0 +1,17 @@
+print("Hello world")
+# this is a single line comment
+
+cars = 5
+driving = True
+
+print("I have %s cars" %cars)
+print("i have" + str(cars) + "cars.")
+
+
+age = input("How old are you")
+print("%s?? Really??? % age" % age)
+
+color = ["Red", "Blue", "Black", "yellow", "green"]
+color.append("cyan")
+
+print(color)
